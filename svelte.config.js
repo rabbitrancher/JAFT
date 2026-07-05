b6e9b08fa14c60814e0e2b35a61be492758745a7
rabbitrancher <150820815+rabbitrancher@users.noreply.github.com>
2026-07-04 20:41:39 -0400

    update npm to not need csrf block

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
-		csrf: {
-			checkOrigin: true
-		}
-	}
+	},
 };
 
 export default config;
